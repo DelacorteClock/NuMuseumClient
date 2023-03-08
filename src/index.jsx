a8a027df498745fa44282bde100e99ab5210ff19
DelacorteClock <123141973+DelacorteClock@users.noreply.github.com>
2023-03-08 12:56:47 -0800

    Added container

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import {createRoot} from 'react-dom/client';
+import Container from 'react-bootstrap/Container';
 import MainView from './components/main-view/main-view';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 const NuMuseumApp = function () {
-    return <MainView />;
+    return <Container><MainView /></Container>;
 };
 
 //Get app root
